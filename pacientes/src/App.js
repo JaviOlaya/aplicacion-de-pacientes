1eb13aec3ebb73a028f383ab02ff8d9b6ef0740f
JaviOlaya <fullstack@javiolaya.online>
2020-12-07 22:45:11 +0100

    first commit

--- pacientes/src/App.js
+++ pacientes/src/App.js
@@ -1,22 +1,37 @@
+import React,{Fragment, useState } from 'react';
+import Formulario from './components/Formulario';
+
 
 function App() {
+
+  // Arreglo de citas
+  const [citas, guardarCitas] = useState([]);
+
+
+  // Funcion que tome las citas actuales y agregue una nueva 
+    const crearCita = cita =>{
+      console.log(cita);
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Fragment>
+     <h1>Administrador de citas de Pacientes</h1>
+
+<div className="container">
+    <div className="row">
+      <div className="one-half column">
+        <Formulario 
+          crearCita={crearCita}
+        />
+      </div>
+      <div className="one-half column">
+          2
+      </div>
     </div>
+</div>
+
+   </Fragment>
+    
+
   );
 }
 
